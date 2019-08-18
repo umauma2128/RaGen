060b31ecc3b39ac8dc7c7bb437eaea743cf1d627
Rahini <rockzrahi@gmail.com>
2019-08-18 12:03:06 +0530

    test 2

--- php/cma-sandbox/w_program_sandbox.php
+++ php/cma-sandbox/w_program_sandbox.php
@@ -59,11 +59,15 @@
     $si = "subnet_ids         = [$subnet_id]";
     $az = "availability_zones = [$avail_zone]";
     
-    
-           mkdir(../output/cma-sandbox/$en, 0777);
+    $path = "../output/cma-sandbox/";
+    $fullpath = $path . "/" . $fn;
+    if (!file_exists($fullPath)){
+        mkdir($fullpath, 0777);}
+    $filename = "main.tf"
+    $file = $fullpath . "/" . $filename
    // chdir('/Applications/XAMPP/xamppfiles/htdocs/php/terraform/env/{$en}/');
-           mkdir(../output/cma-sandbox/$en/$fn, 0777);
-    $file = fopen('../output/cma-sandbox/{$en}/$fn/main.tf', 'a+'); //Open your .txt file
+          // mkdir(../output/cma-sandbox/$en/$fn, 0777);
+    $file = fopen("$file", 'a+'); //Open your .txt file
     ftruncate($file, 0); //Clear the file to 0bit
     $content = $bucket. PHP_EOL ;
     $content1 = $keyy. PHP_EOL ;
