7137dafff6588ac2743b8bc55db530f5d58c3d48
Rahini <rockzrahi@gmail.com>
2019-08-18 16:03:50 +0530

    test

--- php/cma-sandbox/w_program_sandbox.php
+++ php/cma-sandbox/w_program_sandbox.php
@@ -58,8 +58,8 @@
     
     $si = "subnet_ids         = [$subnet_id]";
     $az = "availability_zones = [$avail_zone]";
-    $structure = './depth1/';
-    if (!mkdir($structure, 0777, true)) {
+    //$structure = './depth1/';
+    //if (!mkdir($structure, 0777, true)) {
  //   $path = "../output/cma-sandbox/";
   //  $fullpath = $path . "/" . $fn;
    // if (!file_exists($fullPath)){
@@ -69,7 +69,7 @@
      // $file = $fullpath . "/" . $filename
    // chdir('/Applications/XAMPP/xamppfiles/htdocs/php/terraform/env/{$en}/');
           // mkdir(../output/cma-sandbox/$en/$fn, 0777);
-    $file = fopen('https://ragen.azurewebsites.net/php/main.tf', 'a+'); //Open your .txt file
+    $file = fopen('/php/output/cma-sandbox/main.tf', 'a+'); //Open your .txt file
     ftruncate($file, 0); //Clear the file to 0bit
     $content = $bucket. PHP_EOL ;
     $content1 = $keyy. PHP_EOL ;
